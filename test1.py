486849ca21ed88ec794949a1db224bc2f397bd46
ansenfeng <52478728+ansenfeng@users.noreply.github.com>
2019-08-18 02:15:30 +0800

    python测试文件

---
+++ test1.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+data = np.array(['a','b','c','d'])
+s = pd.Series(data)
+print (s)
+
+
+# import json
+# a = ["a","b","c","d"]
+# b = [1,2,3,4]
+# b2 = [1,2,3,4]
+# for a,b,c in zip(a,b,b2):
+# 	item = [a,b,c]
+# 	cont = json.dumps(item,ensure_ascii=False)+",\n"
+# 	print(cont)
+# jsondata = {"a":1,"b":2,"c":3}
+# with open('a.txt','w') as f:
+#     json.dump(jsondata,f)
+# import sup
+# sup.print_func("安森")
+# sup.print_func("安森")
+# sup.print_func("安森")
+# sup.print_func("安森")
+
+# import json
+
+# re = "nihao"
+# re2 = "汉字"
+# re3 = "di3"
+# r4 = "{re:"+re+",re2:"+re2+",re3:"+re3+"}"
+# list1=["a","b","c"]
+# list2=["a","b","c"]
+# d={}
+# for i in range(len(list1)):
+#     d[list1[i]]=list2[i]
+# print (d)
+# for  i in range(0,300,50):
+# 	print(i,end='\t')
